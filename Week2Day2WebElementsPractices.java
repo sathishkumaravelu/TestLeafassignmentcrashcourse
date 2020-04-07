6e2efcbfc93782176617fc5bd71b5827526660c9
sathish kumaravelu <37692318+sathishkumaravelu@users.noreply.github.com>
2020-04-08 00:47:34 +0530

    Update Week2Day2WebElementsPractices.java
    added the additional lines of code to print the text in label

--- Week2Day2WebElementsPractices.java
+++ Week2Day2WebElementsPractices.java
@@ -1,4 +1,4 @@
-package week2.day1;
+package week2.day2;
 
 import java.util.List;
 
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Week2Day2WebElementsPractices {
 
-	public static void main(String [] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "./Chromedriver.exe");
@@ -18,23 +18,33 @@ public class Week2Day2WebElementsPractices {
 
 		// to store the list of WebElement
 		List<WebElement> listOfLinks = driver.findElementsByTagName("a");
-		// to store the the number of links in int 
-		
+
+		// to store the the number of links in int
 		int noOfLinks = listOfLinks.size();
 		System.out.println("No of hyper links found in page" + noOfLinks);
-		
-		//to find the first matching href (link)
+
+		// to find the first matching href (link) and click it
 		WebElement firstLink = listOfLinks.get(0);
 		firstLink.click();
-	
+		System.out.println("the current page is" + driver.getCurrentUrl());
+
 		driver.navigate().back();
-		driver.navigate().forward();
-	
-		
-		List<WebElement> listOfLinkSecondTime = driver.findElementsByTagName("a");		
-		WebElement newLastLink = listOfLinkSecondTime.get(noOfLinks-1);
+
+		Thread.sleep(3000);
+
+		// driver.navigate().forward();
+
+		// 2. click on last link of the page
+		List<WebElement> listOfLinkSecondTime = driver.findElementsByTagName("a");
+		WebElement newLastLink = listOfLinkSecondTime.get(noOfLinks - 1);
 		newLastLink.click();
-		
-		System.out.println("done");
+
+		/* 3. to print the text near to the go to home page */
+
+		// using sibling downwards xpath it is accessed
+		String textOfLable = driver.findElementByXPath("(//a[text()='Go to Home Page'])[last()]/following-sibling::label").getText();
+		System.out.println(textOfLable);
+
+		System.out.println("class practice for the day done");
 	}
 }
